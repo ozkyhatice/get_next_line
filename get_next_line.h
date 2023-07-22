f5b23e70418b5fceeaa432da70d7edefb7110014
hatice özkaya <hatice@hatice-MacBook.local>
2023-07-22 23:01:59 +0300

    paco-ok

--- get_next_line.h
+++ get_next_line.h
@@ -4,8 +4,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-size_t	ft_strlen(char *str);
-char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
 char	*ft_getafter(char *stash);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,59 +1,59 @@
 #include "get_next_line.h"
 
-size_t	ft_strlen(char *str)
+size_t	ft_strlen(const char *s)
 {
-	size_t	c;
-
-	c = 0;
-	if (!str)
-		return (0);
-	while (str[c] != '\0')
-		c++;
-	return (c);
-}
-
-char	*ft_strchr(char *s, int c)
-{
-	int	i;
+	size_t	i;
 
 	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	i;
+
+	i = (char)c;
 	if (!s)
 		return (0);
-	if (c == '\0')
-		return ((char *)&s[ft_strlen(s)]);
-	while (s[i] != '\0')
+	if (i == '\0')
+		return ((char *)s);
+	while (*s)
 	{
-		if (s[i] == (char) c)
-			return ((char *)&s[i]);
-		i++;
+		if (*s == i)
+			return ((char *)s);
+		s++;
 	}
 	return (0);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
 {
+	char	*ptr;
 	size_t	i;
-	size_t	c;
-	char	*str;
+	size_t	j;
 
 	if (!s1)
 	{
-		s1 = (char *)malloc(1 * sizeof(char));
+		s1 = (char *)malloc(sizeof(char) * 1);
 		s1[0] = '\0';
 	}
-	if (!s1 || !s2)
+	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!ptr)
 		return (NULL);
-	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	i = -1;
-	c = 0;
-	if (s1)
-		while (s1[++i] != '\0')
-			str[i] = s1[i];
-	while (s2[c] != '\0')
-		str[i++] = s2[c++];
-	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+		ptr[i++] = s2[j++];
+	ptr[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	free(s1);
-	return (str);
+	return (ptr);
 }
