c4b299090127e8504e2dad25797825ca627e10a7
hatice özkaya <hatice@hatice-MacBook.local>
2023-07-28 17:42:19 +0300

    define buffer_size

--- get_next_line.h
+++ get_next_line.h
@@ -4,6 +4,10 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
